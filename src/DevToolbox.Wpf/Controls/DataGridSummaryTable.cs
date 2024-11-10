9bab3eb77a047f084e873aa280c2dccf0e27009f
Christos Gkatzos <christos.gkatzos@hotmail.com>
2024-11-10 08:37:53 +0200

    in progress

---
+++ src/DevToolbox.Wpf/Controls/DataGridSummaryTable.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using DevToolbox.Wpf.Data;
+using DevToolbox.Wpf.Extensions;
+using DevToolbox.Wpf.Helpers;
+
+namespace DevToolbox.Wpf.Controls;
+
+/// <summary>
+/// Represents a control that displays summary information for a DataGrid.
+/// </summary>
+public class DataGridSummaryTable : ItemsControl
+{
+    #region Fields/Consts
+
+    private readonly ObservableCollection<DataGridSummaryTableColumn> _columns = [];
+    private GridControl? _gridControl;
+    private ScrollViewer? _scrollViewer;
+
+    #endregion
+
+    static DataGridSummaryTable()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(DataGridSummaryTable), new FrameworkPropertyMetadata(typeof(DataGridSummaryTable)));
+    }
+
+    #region Methods Overrides
+
+    /// <summary>
+    /// Called when the template is applied to the control.
+    /// </summary>
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        if (_scrollViewer is not null)
+        {
+            _scrollViewer.ScrollChanged -= ScrollViewer_ScrollChanged;
+        }
+
+        _scrollViewer = Template.FindName("DG_ScrollViewer", this) as ScrollViewer;
+
+        if (_scrollViewer is not null)
+        {
+            _scrollViewer.ScrollChanged += ScrollViewer_ScrollChanged;
+        }
+
+        if (_gridControl is not null)
+        {
+            _gridControl.Columns.CollectionChanged -= Columns_CollectionChanged;
+            _gridControl.ColumnReordered -= GridControl_ColumnReordered;
+        }
+
+        _gridControl = this.FindVisualAncestor<GridControl>();
+
+        if (_gridControl is not null)
+        {
+            _gridControl.Columns.CollectionChanged += Columns_CollectionChanged;
+            _gridControl.ColumnReordered += GridControl_ColumnReordered;
+        }
+
+        CalculateSummaryValues();
+
+        EventManager.RegisterClassHandler(typeof(DataGridColumnHeader), DataGridColumnHeader.SizeChangedEvent, new SizeChangedEventHandler(ColumnSplitter_SizeChanged));
+
+        void ColumnSplitter_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateSummaries();
+        }
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Computes the summary values for the table and updates the columns.
+    /// </summary>
+    public void CalculateSummaryValues()
+    {
+        if (_gridControl is null || !_gridControl.TotalSummary.Any() || !_gridControl.Columns.Any())
+            return;
+
+        _columns.Clear();
+
+        foreach (var column in _gridControl.Columns.OrderBy(x => x.DisplayIndex))
+        {
+            _columns.Add(new DataGridSummaryTableColumn
+            {
+                ColumnName = column.SortMemberPath,
+                Visibility = column.Visibility,
+                Width = column.ActualWidth
+            });
+        }
+
+        var rowCountByColumn = new Dictionary<string, int>();
+        var itemsSource = GetItemsSourceAsDataTable() ?? throw new ArgumentNullException("itemsSource", "ItemsSource cannot be null.");
+
+        foreach (var gridSummaryItem in _gridControl.TotalSummary)
+        {
+            var showInColumn = gridSummaryItem.ShowInColumn ?? gridSummaryItem.FieldName;
+            var value = DataGridSummaryHelper.GetValue(itemsSource, gridSummaryItem);
+
+            if (!rowCountByColumn.TryGetValue(showInColumn, out int index))
+            {
+                index = 0;
+                rowCountByColumn[showInColumn] = index;
+            }
+            else
+            {
+                rowCountByColumn[showInColumn] = ++index;
+            }
+
+            _columns.First(c => c.ColumnName == showInColumn).Cells.Insert(index, new DataGridSummaryTableCell
+            {
+                Value = value,
+                Style = gridSummaryItem.Style
+            });
+        }
+
+        ItemsSource = _columns;
+    }
+
+    /// <summary>
+    /// Updates the visibility and width of summary columns based on the corresponding columns in the GridControl.
+    /// </summary>
+    private void UpdateSummaries()
+    {
+        if (_gridControl is null) return;
+
+        foreach (var column in _gridControl.Columns)
+        {
+            var summaryRowGridColumn = _columns.FirstOrDefault(c => c.ColumnName == column.SortMemberPath);
+            if (summaryRowGridColumn != null)
+            {
+                summaryRowGridColumn.Visibility = column.Visibility;
+                summaryRowGridColumn.Width = column.ActualWidth;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Converts the ItemsSource to a DataTable for easier summary computation.
+    /// </summary>
+    /// <returns>A DataTable representing the ItemsSource.</returns>
+    private DataTable? GetItemsSourceAsDataTable()
+    {
+        if (_gridControl?.ItemsSource is DataView dataView)
+        {
+            return dataView.ToTable();
+        }
+        else if (_gridControl?.ItemsSource != null)
+        {
+            return DataTableHelper.ToDataTable(_gridControl.ItemsSource.Cast<object>());
+        }
+        else if (_gridControl?.Items != null)
+        {
+            return DataTableHelper.ToDataTable(_gridControl.Items.Cast<object>());
+        }
+
+        return null;
+    }
+
+    #endregion
+
+    #region Events Subscriptions
+
+    /// <summary>
+    /// Handles the ScrollChanged event of the ScrollViewer to synchronize scrolling with the GridControl.
+    /// </summary>
+    private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+    {
+        if (e.HorizontalChange == 0)
+        {
+            return;
+        }
+
+        if (_gridControl?.ScrollViewer is null)
+        {
+            return;
+        }
+
+        UpdateLayout();
+        _gridControl.ScrollViewer.ScrollToHorizontalOffset(e.HorizontalOffset);
+    }
+
+    /// <summary>
+    /// Handles the ColumnReordered event to recompute the summary values.
+    /// </summary>
+    private void GridControl_ColumnReordered(object? sender, DataGridColumnEventArgs e)
+    {
+        CalculateSummaryValues();
+    }
+
+    /// <summary>
+    /// Handles the CollectionChanged event of the GridControl columns to recompute the summary values.
+    /// </summary>
+    private void Columns_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        CalculateSummaryValues();
+    }
+
+    #endregion
+}
---
+++ src/DevToolbox.Wpf/Controls/DataGridSummaryTableColumnsPresenter.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using DevToolbox.Wpf.Controls.Utils;
+using DevToolbox.Wpf.Extensions;
+
+namespace DevToolbox.Wpf.Controls;
+
+internal class DataGridSummaryTableColumnsPresenter : VirtualizingStackPanel
+{
+    #region Fields/Consts
+
+    private ScrollBar? _horizontalScrollBar;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the owner of the DataGridRowsPresenter, cast as a GridControl.
+    /// </summary>
+    private GridControl? Owner => this?.FindVisualAncestor<GridControl>();
+
+    /// <summary>
+    /// Gets the list of realized (visible) child elements in the VirtualizingStackPanel.
+    /// </summary>
+    private IList? GetRealizedChildren => typeof(VirtualizingStackPanel).InvokeMember(
+        "RealizedChildren",
+        BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetProperty,
+        null, this, null) as IList;
+
+    /// <summary>
+    /// Gets the vertical ScrollBar used in the DataGrid.
+    /// </summary>
+    private ScrollBar? HorizontalScrollBar
+    {
+        get
+        {
+            if (_horizontalScrollBar == null)
+            {
+                _horizontalScrollBar = Owner?.FindVisualChildByName<ScrollBar>("PART_HorizontalScrollBar");
+                _horizontalScrollBar?.UpdateLayout();
+            }
+            return _horizontalScrollBar;
+        }
+    }
+
+    #endregion
+
+    #region Methods Overrides
+
+    protected override Size ArrangeOverride(Size arrangeSize)
+    {
+        if (Owner == null || Owner.FrozenColumnCount <= 0)
+        {
+            return base.ArrangeOverride(arrangeSize);
+        }
+
+        var rcChild = new Rect(arrangeSize);
+        var frozenColumnCount = Owner.FrozenColumnCount;
+        var nextFrozenColumnStart = 0.0;
+        var nextNonFrozenColumnStart = 0.0;
+
+        // Determine horizontal offset and physical offset for arranging children
+        double physicalOffset = 0.0;
+        var originPoint = new Point(0, 0);
+
+        if (this is IScrollInfo scrollInfo)
+        {
+            var horizontalOffset = scrollInfo.HorizontalOffset;
+            physicalOffset = -ComputePhysicalFromLogicalOffset(horizontalOffset, true);
+            rcChild.Y = -scrollInfo.VerticalOffset;
+        }
+
+        var sbOffset = HorizontalScrollBar?.TransformToAncestor(Owner)?.Transform(originPoint).X ?? 0;
+        var rowsPanelOffset = TransformToAncestor(Owner)?.Transform(originPoint).X ?? 0;
+        var viewportStartX = sbOffset - rowsPanelOffset;
+        nextNonFrozenColumnStart = viewportStartX - physicalOffset;
+
+        // Arrange children
+        var children = GetRealizedChildren ?? new List<UIElement>();
+        for (int i = 0; i < children.Count; ++i)
+        {
+            if (children[i] is not UIElement container)
+                continue;
+
+            var childSize = container.DesiredSize;
+            rcChild.Height = arrangeSize.Height;
+            rcChild.Width = childSize.Width;
+
+            if (i < frozenColumnCount)
+            {
+                // Arrange frozen columns
+                rcChild.X = nextFrozenColumnStart;
+                nextFrozenColumnStart += childSize.Width;
+            }
+            else
+            {
+                // Arrange non-frozen columns
+                if (DoubleUtil.LessThan(nextNonFrozenColumnStart, nextFrozenColumnStart))
+                {
+                    var columnChoppedWidth = nextFrozenColumnStart - nextNonFrozenColumnStart;
+                    if (DoubleUtil.AreClose(columnChoppedWidth, 0.0))
+                    {
+                        nextNonFrozenColumnStart = nextFrozenColumnStart + childSize.Width;
+                    }
+                    else
+                    {
+                        var clipWidth = childSize.Width - columnChoppedWidth;
+                        nextNonFrozenColumnStart = nextFrozenColumnStart + clipWidth;
+                    }
+                    rcChild.X = nextFrozenColumnStart;
+                }
+                else
+                {
+                    rcChild.X = nextNonFrozenColumnStart;
+                    nextNonFrozenColumnStart += childSize.Width;
+                }
+            }
+
+            container.Arrange(rcChild);
+        }
+
+        return arrangeSize;
+    }
+
+    /// <summary>
+    /// Called when the viewport offset changes, typically due to scrolling.
+    /// This method invalidates the arrangement if the vertical offset has changed.
+    /// </summary>
+    /// <param name="oldViewportOffset">The old viewport offset.</param>
+    /// <param name="newViewportOffset">The new viewport offset.</param>
+    protected override void OnViewportOffsetChanged(Vector oldViewportOffset, Vector newViewportOffset)
+    {
+        base.OnViewportOffsetChanged(oldViewportOffset, newViewportOffset);
+
+        if (!DoubleUtil.AreClose(oldViewportOffset.Y, newViewportOffset.Y))
+        {
+            InvalidateArrange();
+        }
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Computes the physical offset from the logical offset.
+    /// This is used to convert logical scrolling positions to physical pixel offsets.
+    /// </summary>
+    /// <param name="logicalOffset">The logical offset (e.g., the scroll position).</param>
+    /// <param name="fHorizontal">Indicates whether the offset is for horizontal scrolling.</param>
+    /// <returns>The computed physical offset in pixels.</returns>
+    private double ComputePhysicalFromLogicalOffset(double logicalOffset, bool fHorizontal)
+    {
+        var physicalOffset = 0.0;
+        var children = GetRealizedChildren ?? new List<UIElement>();
+
+        for (var i = 0; i < Math.Min(logicalOffset, children.Count); i++)
+        {
+            if (children[i] is UIElement child)
+            {
+                physicalOffset -= fHorizontal ? child.DesiredSize.Width : child.DesiredSize.Height;
+            }
+        }
+
+        return physicalOffset;
+    }
+
+    #endregion
+}
