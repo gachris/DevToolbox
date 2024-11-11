e25996a41550233af05dfb7fa6ef653022a6c75a
Christos Gkatzos <christos.gkatzos@hotmail.com>
2024-11-11 20:01:51 +0200

    in progress

--- src/DevToolbox.Wpf.Demo/Windows/NotificationWindow.xaml.cs
+++ src/DevToolbox.Wpf.Demo/Windows/NotificationWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using DevToolbox.Wpf.Windows;
+﻿using System.Windows.Controls;
+using System.Windows;
+using DevToolbox.Wpf.Windows;
 
 namespace DevToolbox.Wpf.Demo.Windows;
 
@@ -7,5 +9,16 @@ public partial class NotificationWindow : SidePanelWindow
     public NotificationWindow()
     {
         InitializeComponent();
+    }
+
+    private void SubmitButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Retrieve the input values
+        string name = NameTextBox.Text;
+        string language = (LanguageComboBox.SelectedItem as ComboBoxItem)?.Content.ToString() ?? "None";
+        bool isSubscribed = SubscribeCheckBox.IsChecked ?? false;
+
+        // Display the result
+        ResultTextBlock.Text = $"Hello, {name}! You selected {language}.\nSubscribed: {isSubscribed}";
     }
 }
