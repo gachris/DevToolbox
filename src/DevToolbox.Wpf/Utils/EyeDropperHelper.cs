67df32d4870b71916bb6b1dee1c72fa69f309d9a
Christos Gkatzos <christos.gkatzos@hotmail.com>
2024-11-16 14:53:02 +0200

    fixes

--- src/DevToolbox.Wpf/Utils/EyeDropperHelper.cs
+++ src/DevToolbox.Wpf/Utils/EyeDropperHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing.Drawing2D;
+﻿using System;
+using System.Drawing.Drawing2D;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Media;
 using ColorSpace.Net;
@@ -12,6 +14,11 @@ internal class EyeDropperHelper
 {
     private static readonly IColorConverter<Hsv> _hsvConverter;
     private static readonly IColorConverter<Hsl> _hslConverter;
+
+    private static readonly Regex RgbPattern = new(@"\((R|G|B|r|g|b)\)");
+    private static readonly Regex HsvPattern = new(@"\((H|S|V|h|s|v)\)");
+    private static readonly Regex HslPattern = new(@"\((H|S|L|h|s|l)\)");
+    private static readonly Regex LongPattern = new(@"\((long)\)", RegexOptions.IgnoreCase);
 
     static EyeDropperHelper()
     {
@@ -67,56 +74,147 @@ internal class EyeDropperHelper
         return new System.Drawing.Point((int)x, (int)y);
     }
 
-    public static string GetFormattedColor(SolidColorBrush brush, ColorFormat colorFormat)
+    public static string GetFormattedColor(SolidColorBrush brush, ColorFormat colorFormat, string template, int precision)
     {
-        string? formattedColor;
+        if (precision < 1) precision = 1;
+        if (precision > 11) precision = 11;
+
+        string ReplaceHexPlaceholders(string input)
+        {
+            return RgbPattern.Replace(input, match =>
+            {
+                var component = match.Groups[1].Value[0];
+                var value = component switch
+                {
+                    'R' or 'r' => brush.Color.R,
+                    'G' or 'g' => brush.Color.G,
+                    'B' or 'b' => brush.Color.B,
+                    _ => throw new InvalidOperationException("Invalid color component")
+                };
+                return char.IsUpper(component) ? value.ToString("X2") : value.ToString("x2");
+            });
+        }
+
+        string ReplaceRgbPlaceholders(string input)
+        {
+            return RgbPattern.Replace(input, match =>
+            {
+                var component = match.Groups[1].Value[0];
+                var value = component switch
+                {
+                    'R' or 'r' => brush.Color.R,
+                    'G' or 'g' => brush.Color.G,
+                    'B' or 'b' => brush.Color.B,
+                    _ => throw new InvalidOperationException("Invalid color component")
+                };
+                return value.ToString();
+            });
+        }
+
+        string ReplaceFloatPlaceholders(string input)
+        {
+            return RgbPattern.Replace(input, match =>
+            {
+                var component = match.Groups[1].Value;
+                var value = component switch
+                {
+                    "R" or "r" => brush.Color.ScR,
+                    "G" or "g" => brush.Color.ScG,
+                    "B" or "b" => brush.Color.ScB,
+                    _ => throw new InvalidOperationException("Invalid color component")
+                };
+                return value.ToString($"F{precision}", System.Globalization.CultureInfo.InvariantCulture);
+            });
+        }
+
+        string ReplaceHsvPlaceholders(string input)
+        {
+            var rgb = Rgb.FromRgb(brush.Color.R, brush.Color.G, brush.Color.B);
+            var hsv = _hsvConverter.ConvertFrom(rgb);
+            return HsvPattern.Replace(input, match =>
+            {
+                var component = match.Groups[1].Value[0];
+                var value = component switch
+                {
+                    'H' or 'h' => hsv.H,
+                    'S' or 's' => hsv.S * 100,
+                    'V' or 'v' => hsv.V * 100,
+                    _ => throw new InvalidOperationException("Invalid color component")
+                };
+                return value.ToString($"N0", System.Globalization.CultureInfo.InvariantCulture);
+            });
+        }
+
+        string ReplaceHslPlaceholders(string input)
+        {
+            var rgb = Rgb.FromRgb(brush.Color.R, brush.Color.G, brush.Color.B);
+            var hsl = _hslConverter.ConvertFrom(rgb);
+            return HslPattern.Replace(input, match =>
+            {
+                var component = match.Groups[1].Value[0];
+                var value = component switch
+                {
+                    'H' or 'h' => hsl.H,
+                    'S' or 's' => hsl.S,
+                    'L' or 'l' => hsl.L,
+                    _ => throw new InvalidOperationException("Invalid color component")
+                };
+                return value.ToString($"N0", System.Globalization.CultureInfo.InvariantCulture);
+            });
+        }
+
+        string ReplaceLongPlaceholder(string input)
+        {
+            var colorValue = (brush.Color.R << 16) | (brush.Color.G << 8) | brush.Color.B;
+            return LongPattern.Replace(input, _ => colorValue.ToString());
+        }
 
         switch (colorFormat)
         {
             case ColorFormat.DelphiHex:
-                formattedColor = string.Format(
-                    "${0:X2}{1:X2}{2:X2}{3:X2}",
-                    0,
-                    brush.Color.B,
-                    brush.Color.G,
-                    brush.Color.R);
-                break;
+                if (string.IsNullOrEmpty(template))
+                    template = "$00(b)(g)(r)";
+                return Regex.Unescape(ReplaceHexPlaceholders(template));
+
             case ColorFormat.VBHex:
-                formattedColor = string.Format(
-                    "&H{0:X2}{1:X2}{2:X2}{3:X2}",
-                    0,
-                    brush.Color.B,
-                    brush.Color.G,
-                    brush.Color.R);
-                break;
+                if (string.IsNullOrEmpty(template))
+                    template = "&H00(B)(G)(R)";
+                return Regex.Unescape(ReplaceHexPlaceholders(template));
+
             case ColorFormat.RGB:
-                formattedColor = string.Format($"{brush.Color.R}, {brush.Color.G}, {brush.Color.B}");
-                break;
+                if (string.IsNullOrEmpty(template))
+                    template = "(r), (g), (b)";
+                return Regex.Unescape(ReplaceRgbPlaceholders(template));
+
             case ColorFormat.RGBFloat:
-                formattedColor = string.Format($"{brush.Color.ScR}, {brush.Color.ScG}, {brush.Color.ScB}");
-                break;
+                if (string.IsNullOrEmpty(template))
+                    template = "(r), (g), (b)";
+                return Regex.Unescape(ReplaceFloatPlaceholders(template));
+
             case ColorFormat.HSV:
-                {
-                    var rgb = Rgb.FromRgb(brush.Color.R, brush.Color.G, brush.Color.B);
-                    formattedColor = _hsvConverter.ConvertFrom(rgb).ToString();
-                    break;
-                }
+                if (string.IsNullOrEmpty(template))
+                    template = "(h), (s), (v)";
+                return Regex.Unescape(ReplaceHsvPlaceholders(template));
+
             case ColorFormat.HSL:
-                {
-                    var rgb = Rgb.FromRgb(brush.Color.R, brush.Color.G, brush.Color.B);
-                    formattedColor = _hslConverter.ConvertFrom(rgb).ToString();
-                    break;
-                }
+                if (string.IsNullOrEmpty(template))
+                    template = "(h), (s), (l)";
+                return Regex.Unescape(ReplaceHslPlaceholders(template));
+
             case ColorFormat.Long:
-                formattedColor = (brush.Color.R << 16 | brush.Color.G << 8 | brush.Color.B).ToString();
-                break;
+                if (string.IsNullOrEmpty(template))
+                    template = "(long)";
+                return Regex.Unescape(ReplaceLongPlaceholder(template));
+
             case ColorFormat.HTML:
+                if (string.IsNullOrEmpty(template))
+                    template = "#(R)(G)(B)";
+                return Regex.Unescape(ReplaceHexPlaceholders(template));
             case ColorFormat.Hex:
             default:
-                formattedColor = $"#{brush.Color.R:X2}{brush.Color.G:X2}{brush.Color.B:X2}";
-                break;
+                if (string.IsNullOrEmpty(template))
+                    template = "0x(B)(G)(R)";
+                return Regex.Unescape(ReplaceHexPlaceholders(template));
         }
-
-        return formattedColor;
     }
 }
