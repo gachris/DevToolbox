2e6fffce553af3e809ea167364c69939b1527fb1
Christos Gkatzos <christos.gkatzos@hotmail.com>
2025-06-21 14:18:55 +0300

    interop

--- src/DevToolbox.Wpf/Interop/Dwmapi.cs
+++ src/DevToolbox.Wpf/Interop/Dwmapi.cs
@@ -6,7 +6,7 @@ namespace DevToolbox.Wpf.Interop;
 internal static class Dwmapi
 {
     [DllImport("dwmapi.dll")]
-    public static extern int DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE dwAttribute, ref int pvAttribute, int cbAttribute);
+    public static extern int DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE dwAttribute, ref uint pvAttribute, uint cbAttribute);
 
     [DllImport("dwmapi.dll", PreserveSig = true)]
     public static extern int DwmExtendFrameIntoClientArea(IntPtr hwnd, ref MARGINS margins);
---
+++ src/DevToolbox.Wpf/Interop/NCCALCSIZE_PARAMS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DevToolbox.Wpf.Interop;
+
+internal struct NCCALCSIZE_PARAMS
+{
+    public RECT rgrc0;
+
+    public RECT rgrc1;
+
+    public RECT rgrc2;
+
+    public IntPtr lppos;
+}
